683e1c9b4e2a66338ff4976b71a536ec4aa58b32
WisniowskiPiotr <wisniowski.piotr.89@gmail.com>
2017-02-09 23:30:11 +0100

    added sleep to better render webs

--- Esky.sh
+++ Esky.sh
@@ -20,33 +20,32 @@ do
 	while read dest           
 	do           
 		dest=$(echo $dest | sed "s/ - .*//" )
-		while read date_from           
+		while read date        
 		do           
-			date_from=$(echo $date_from | sed "s/ - .*$//" )
-			while read date_dest           
-			do           
-				date_dest=$(echo $date_dest | sed "s/^.* - //" )
+			date_from=$(echo $date | sed "s/ - .*$//" )       
+			date_dest=$(echo $date | sed "s/^.* - //" )
 
-				tmp_file=$base_path/site
+			tmp_file=$base_path/Esky_$from-$dest-$date_from-$date_dest.html
 
-				if [ $ot = "RoundTrip" ]; then
-					basic_query="https://www.esky.pl/flights/select/?ot=$ot&sc=$sc&tr[0][d]=($from)&tr[1][d]=($dest)&tr[0][a]=($dest)&tr[1][a]=($from)&tr[0][dd]=$date_from&tr[1][dd]=$date_dest&pa=$pa&py=$py&pc=$pc&pi=$pi"
-				else
-					basic_query="https://www.esky.pl/flights/select/?ot=$ot&sc=$sc&tr[0][d]=($from)&tr[0][a]=($dest)&tr[0][dd]=$date_from&pa=$pa&py=$py&pc=$pc&pi=$pi"
-				fi
+			if [ $ot = "RoundTrip" ]; then
+				basic_query="https://www.esky.pl/flights/select/?ot=$ot&sc=$sc&tr[0][d]=($from)&tr[1][d]=($dest)&tr[0][a]=($dest)&tr[1][a]=($from)&tr[0][dd]=$date_from&tr[1][dd]=$date_dest&pa=$pa&py=$py&pc=$pc&pi=$pi"
+			else
+				basic_query="https://www.esky.pl/flights/select/?ot=$ot&sc=$sc&tr[0][d]=($from)&tr[0][a]=($dest)&tr[0][dd]=$date_from&pa=$pa&py=$py&pc=$pc&pi=$pi"
+			fi
 
-				rm -f -r $tmp_file
-				phantomjs $base_path/save_page.js $basic_query >> $tmp_file
-				echo "Found on Esky $ot $from to $dest on $date_from - $date_dest price $price"
+			rm -f -r $tmp_file
+			phantomjs $base_path/save_page.js $basic_query >> $tmp_file
+			
 
-				price1=$(grep -i 'flight-details' $tmp_file | Get_tag.sh span amount 1 )
-				price2=$(grep -i 'flight-details' $tmp_file | Get_tag.sh span amount 2 )
-				price=$((price1+price2))
-				echo $price
-				if [ $price -le $price_limit ]; then
-					echo "Found on Esky $ot $from to $dest on $date_from - $date_dest price $price"
-				fi
-			done <$base_path/$dates
+			price1=$(grep -i 'flight-details' $tmp_file | ./Get_tag.sh span amount 1 )
+			price2=$(grep -i 'flight-details' $tmp_file | ./Get_tag.sh span amount 2 )
+			price=$((price1+price2))
+			echo "Searching on Esky $ot $from to $dest on $date_from - $date_dest - $price"
+			#echo $price
+			#if [ $price -le $price_limit ]; then
+			#	echo "Found on Esky ---------------------------- price $price"
+			#fi
+		
 		done <$base_path/$dates
 	done <$base_path/$towns_end
 done <$base_path/$towns_start 
--- save_page.js
+++ save_page.js
@@ -1,8 +1,16 @@
 var system = require('system');
 var page = require('webpage').create();
 
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  while((new Date().getTime() - start) < milliseconds){}
+  
+}
+
 page.open(system.args[1], function()
 {
+    sleep(30000);
+    page.render('scotch.png');
     console.log(page.content);
     phantom.exit();
 });
